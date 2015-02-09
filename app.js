e806728db4989ca3aadc31275e8c4f85b183b457
ximwang <wangximingde@163.com>
2015-02-09 10:39:41 +0800

    update passport

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var app = express();
 
 // uncomment after placing your favicon in /public
