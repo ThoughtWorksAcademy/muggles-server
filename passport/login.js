c5296c02d5858670c18db249c9e0e6e9b98466ce
ximwang <wangximingde@163.com>
2015-03-02 16:16:50 +0800

    fix passport login

--- passport/login.js
+++ passport/login.js
@@ -4,31 +4,42 @@ var User = mongoose.model('User');
 
 
 module.exports = function (passport) {
+  //passport.use(new LocalStrategy(
+  //  function(username, password, done) {
+  //    process.nextTick(function () {
+  //
+  //      User.findOne({'username': username}, function(err, user) {
+  //        if (err) { return done(err); }
+  //        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+  //        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+  //        return done(null, user);
+  //      })
+  //    });
+  //  }
+  //));
   passport.use('login', new LocalStrategy(
-    function (username, password, done) {
-      console.log('enter passport login');
-      var user = new User();
-      user.password = 'sigh';
-      user.username = 'sigh';
-      user.save();
+    function (res, username, password, done) {
+      //console.log('enter passport login');
+      //var user = new User();
+      //user.password = 'sigh';
+      //user.username = 'sigh';
+      //user.save();
 
-      User.findOne({'username': username},
-        function (err, user) {
+      process.nextTick(function () {
+
+        User.findOne({'username': username}, function (err, user) {
           if (err) {
-            console.log('err');
             return done(err);
           }
-
           if (!user) {
-            return done(null, false);
+            return done(null, false, {message: 'Unknown user ' + username});
           }
-
-          if (!isValidPassword(user, password)) {
-            return done(null, false);
+          if (user.password != password) {
+            return done(null, false, {message: 'Invalid password'});
           }
-
           return done(null, user);
         })
+      });
     }));
 
   var isValidPassword = function (user, password) {
--- router/routes/users.js
+++ router/routes/users.js
@@ -3,15 +3,37 @@ var express = require('express');
 var router = express.Router();
 var Checkpoint = mongoose.model('Checkpoint');
 var User = mongoose.model('User');
-
 var _ = require('lodash');
-
+var LocalStrategy = require('passport-local').Strategy;
 module.exports = function (passport) {
 
-  router.post('/login', passport.authenticate('login', {failureRedirect: '/'}),
-  function(req, res) {
-    res.send(req.user);
-    console.log('登录成功');
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      process.nextTick(function () {
+        User.findOne({'username': username}, function(err, user) {
+          if (err) { return done(err); }
+          if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+          if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+          return done(null, user);
+        })
+      });
+    }
+  ));
+
+  router.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+      if (err) { return next(err) }
+
+      if (!user) {
+        return res.status(401).send(info.message);
+      }
+
+      req.logIn(user, function(err) {
+        if (err) { return next(err); }
+        return res.send(user);
+      });
+
+    })(req, res, next);
   });
 
   //router.post('/signup',
