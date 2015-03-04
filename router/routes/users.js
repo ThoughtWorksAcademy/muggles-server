11b89f848654b41871818d95e36b492e280994f6
ximwang <wangximingde@163.com>
2015-03-04 22:42:20 +0800

    get course from db right

--- router/routes/users.js
+++ router/routes/users.js
@@ -136,17 +136,50 @@ module.exports = function (passport) {
   });
 
   router.get('/courses', function (req, res) {
-    console.log('api/user/courses');
+    //console.log('api/user/courses');
     //var course = new Course();
     //
     //course.name = '面向对象checkpoints';
-    //course.checkpoints.push('54f682ffe6f9fac64166fd18');
-    //course.checkpoints.push('54f682ffe6f9fac64166fd17');
-    //course.checkpoints.push('54f682ffe6f9fac64166fd16');
-    //course.checkpoints.push('54f682ffe6f9fac64166fd15');
-    //course.checkpoints.push('54f682ffe6f9fac64166fd14');
-    //course.checkpoints.push('54f682ffe6f9fac64166fd13');
-    //course.checkpoints.push('54f682ffe6f9fac64166fd12');
+    //var CheckpointType = mongoose.model('CheckpointType');
+    //var checkpointType = new CheckpointType();
+    //checkpointType.name = '默认类型';
+    //var checkpoint = new Checkpoint();
+    //checkpoint.name = '牛顿第一定律';
+    //checkpoint.type = checkpointType._id;
+    //checkpoint.save(function(err) {
+    //  if(err) {
+    //    console.log('save checkpoint err' + err);
+    //    throw err;
+    //  }
+    //});
+    //
+    //var checkpoint2 = new Checkpoint();
+    //checkpoint2.type = checkpointType._id;
+    //
+    //checkpoint2.name = '牛顿第二定律';
+    //checkpoint2.save(function(err) {
+    //  if(err) {
+    //    console.log('save checkpoint err' + err);
+    //    throw err;
+    //  }
+    //});
+    //
+    //var checkpoint3 = new Checkpoint();
+    //checkpoint3.name = '牛顿第三定律';
+    //checkpoint3.type = checkpointType._id;
+    //
+    //checkpoint3.save(function(err) {
+    //  if(err) {
+    //    console.log('save checkpoint err' + err);
+    //    throw err;
+    //  }
+    //});
+    //
+    //course.checkpoints.push(checkpoint);
+    //course.checkpoints.push(checkpoint2);
+    //course.checkpoints.push(checkpoint3);
+    //
+    //
     //course.save(function (err) {
     //  if(err) {
     //    throw err;
