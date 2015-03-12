1583808179021203e957240170ba4008d680f4ca
yfwang <yufei930101@gmail.com>
2015-03-13 00:50:47 +0800

    add stations router

--- router/index.js
+++ router/index.js
@@ -1,5 +1,5 @@
 module.exports = function(app, passport) {
-  app.use('/api/categories', require('./routes/categories'));
+  app.use('/api/stations', require('./routes/stations'));
   app.use('/api/users', require('./routes/users')(passport));
   app.use('/api/trainees', require('./routes/trainees')(passport));
   app.use('/api/courses', require('./routes/courses'));
---
+++ router/routes/stations.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Station = mongoose.model('Station');
+
+router.post('/', function (req, res) {
+  Station.create({
+    name: '欧亚学院',
+    courses: ['5501c04880d1550e545e14fc', '5501c08a048fe75454dd6d26', '5501c0ae0084557e54cebac5'],
+    students: ['5501beeec2b53a7a526008ab', '5501bf0d41c18ca5521fc573']
+  }, function (err, station) {
+    res.send(station);
+  });
+});
+
+module.exports = router;
