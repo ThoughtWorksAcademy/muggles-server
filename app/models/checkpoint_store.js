6b6cd2629bf3e9ff64b73d06bbaf26aa04431143
yfwang <yufei930101@gmail.com>
2015-03-13 00:04:01 +0800

    delete checkpoint_store.js

--- app/models/checkpoint_store.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Checkpoint = new Schema({
-  name: String,
-  type: {type: Schema.ObjectId, ref: 'CheckpointType'}
-});
-mongoose.model('CheckpointStore', Checkpoint);
