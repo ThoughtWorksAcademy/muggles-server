9d5db194fcc741d6a3ef1d99b66f5540ccea4f48
ximwang <wangximingde@163.com>
2015-03-13 10:28:10 +0800

    fix stations model bug

--- app/models/station.js
+++ app/models/station.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var StationSchema = new Schema({
   name: String,
   courses: [{type: Schema.ObjectId, ref: 'Course'}],
-  trainees: [{type: Schema.ObjectId, ref: 'Trainer'}]
+  trainees: [{type: Schema.ObjectId, ref: 'Trainee'}]
 });
 
 mongoose.model('Station', StationSchema);
