2de9726477a1d81d64d80ae85d2e188df0b34244
ximwang <wangximingde@163.com>
2015-03-13 10:03:37 +0800

    add trainers login route

--- router/index.js
+++ router/index.js
@@ -3,6 +3,6 @@ module.exports = function(app, passport) {
   app.use('/api/users', require('./routes/users')(passport));
   app.use('/api/trainees', require('./routes/trainees')(passport));
   app.use('/api/courses', require('./routes/courses'));
-  app.use('/api/trainers', require('./routes/trainers'));
+  app.use('/api/trainers', require('./routes/trainers')(passport));
   app.use('/api/checkpoints', require('./routes/checkpoints'));
 };
