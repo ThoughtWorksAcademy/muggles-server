e43d6a4947836dd902dc625024bfd52890408651
ximwang <wangximingde@163.com>
2015-04-29 14:54:14 +0800

    fix user schema

---
+++ models/admin.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var AdminSchema = new Schema({
+  username: String,
+  password: String,
+  email: String
+});
+
+mongoose.model('Admin', AdminSchema);
--- models/trainer.js
+++ models/trainer.js
@@ -4,6 +4,12 @@ var Schema = mongoose.Schema;
 var TrainerSchema = new Schema({
   username: String,
   password: String,
+  email: String,
+  sex: String,
+  create_date: String,
+  image: String,
+  phone_number: String,
+  groups: [],
   stations: [{type: Schema.ObjectId, ref: 'Station'}]
 });
 
