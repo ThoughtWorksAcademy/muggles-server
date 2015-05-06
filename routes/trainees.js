f1e721c054d08343a86eb51459dca71861c9eb64
ximwang <wangximingde@163.com>
2015-05-06 16:58:11 +0800

    fix route/trainee

--- routes/trainees.js
+++ routes/trainees.js
@@ -5,110 +5,104 @@ var User = mongoose.model('User');
 var Trainee = mongoose.model('Trainee');
 
 var REGISTER_SUCCESS = '注册成功';
+var Course = mongoose.model('Course');
+var LocalStrategy = require('passport-local').Strategy;
 
-router.get('/:email', function(req, res, next) {
+module.exports = function (passport) {
 
-  var email = req.params.email;
+  passport.use('trainee', new LocalStrategy(
+    function (username, password, done) {
+      process.nextTick(function () {
 
-  Trainee.findOne({email: email})
-    .exec()
-    .then(function(trainee) {
+        Trainee.findOne({'username': username}, function (err, user) {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false, {message: '无法找到用户: ' + username});
+          }
+          if (user.password != password) {
+            return done(null, false, {message: '密码错误'});
+          }
+          return done(null, user);
+        })
+      });
+    }
+  ));
 
-      if(trainee) {
-
-        res.send({state: 200, data: false, message: '邮箱可用'})
-      } else {
-
-        res.send({state: 200, data: true, message: '邮箱已被注册'})
+  router.post('/login', function (req, res, next) {
+    passport.authenticate('trainee', function (err, user, info) {
+      if (err) {
+        return next(err)
       }
-    })
-    .onReject(function(err) {
 
-      next(err);
-    })
-});
+      if (!user) {
+        return res.status(401).send(info.message);
+      }
 
-router.post('/', function(req, res, next) {
+      req.logIn(user, function (err) {
+        if (err) {
+          return next(err);
+        }
+        return res.send(user);
+      });
 
-  var trainee = req.body;
+    })(req, res, next);
+  });
 
-  Trainee.create(trainee)
-    .then(function(trainee) {
+  router.get('/', function (req, res) {
+    Trainee.find({}, function (err, trainees) {
+      res.send(trainees);
+    });
+  });
 
-      res.send({state: 200, data: trainee, message: REGISTER_SUCCESS})
-    })
-    .onReject(function(err) {
+  router.get('/:id/courses/', function (req, res) {
 
-      next(err);
-    })
-});
+    Trainee.findById(req.params.id)
+      .populate('courses.course')
+      .populate('courses.trainer', 'username')
+      .populate('courses.sponsor', 'username')
+      .exec(function (err, trainee) {
+        res.send(trainee.courses);
+      });
+  });
 
-module.exports = router;
-//var Course = mongoose.model('Course');
-//var LocalStrategy = require('passport-local').Strategy;
-//
-//module.exports = function (passport) {
-//
-//  passport.use('trainee', new LocalStrategy(
-//    function (username, password, done) {
-//      process.nextTick(function () {
-//
-//        Trainee.findOne({'username': username}, function (err, user) {
-//          if (err) {
-//            return done(err);
-//          }
-//          if (!user) {
-//            return done(null, false, {message: '无法找到用户: ' + username});
-//          }
-//          if (user.password != password) {
-//            return done(null, false, {message: '密码错误'});
-//          }
-//          return done(null, user);
-//        })
-//      });
-//    }
-//  ));
-//
-//  router.post('/login', function (req, res, next) {
-//    passport.authenticate('trainee', function (err, user, info) {
-//      if (err) {
-//        return next(err)
-//      }
-//
-//      if (!user) {
-//        return res.status(401).send(info.message);
-//      }
-//
-//      req.logIn(user, function (err) {
-//        if (err) {
-//          return next(err);
-//        }
-//        return res.send(user);
-//      });
-//
-//    })(req, res, next);
-//  });
-//
-//  router.get('/', function (req, res) {
-//    Trainee.find({}, function (err, trainees) {
-//      res.send(trainees);
-//    });
-//  });
-//
-//  router.get('/:id/courses/', function (req, res) {
-//
-//    Trainee.findById(req.params.id)
-//      .populate('courses.course')
-//      .populate('courses.trainer', 'username')
-//      .populate('courses.sponsor', 'username')
-//      .exec(function (err, trainee) {
-//        res.send(trainee.courses);
-//      });
-//  });
-//
-//  router.patch('course/checkpoints/', function () {
-//
-//  });
-//
-//  return router;
-//};
+  router.get('/:email', function (req, res, next) {
+
+    var email = req.params.email;
+
+    Trainee.findOne({email: email})
+      .exec()
+      .then(function (trainee) {
+
+        if (trainee) {
+
+          res.send({state: 200, data: false, message: '邮箱可用'})
+        } else {
+
+          res.send({state: 200, data: true, message: '邮箱已被注册'})
+        }
+      })
+      .onReject(function (err) {
+
+        next(err);
+      })
+  });
+  
+  router.post('/', function (req, res, next) {
+
+    var trainee = req.body;
+
+    Trainee.create(trainee)
+      .then(function (trainee) {
+
+        res.send({state: 200, data: trainee, message: REGISTER_SUCCESS})
+      })
+      .onReject(function (err) {
+
+        next(err);
+      })
+  });
+
+  return router;
+};
