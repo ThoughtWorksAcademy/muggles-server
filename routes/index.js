0ad6c35a7eb13f7386f1dd67f637ac76f08ece67
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-06 14:08:34 +0800

    add trainees routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 module.exports = function (app, passport) {
   app.use('/api/stations', require('./stations'));
   app.use('/api/users', require('./users')(passport));
-  app.use('/api/trainees', require('./trainees')(passport));
+  //app.use('/api/trainees', require('./trainees')(passport));
+  app.use('/api/trainees', require('./trainees'));
   app.use('/api/courses', require('./courses'));
   app.use('/api/trainers', require('./trainers')(passport));
   app.use('/api/checkpoints', require('./checkpoints'));
+  app.use('/api/invitation', require('./invitation-code'));
 };
---
+++ routes/invitation-code.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const ERROR_CODE = '请填写正确的邀请码';
+const CORRECT_CODE = '邀请码输入正确';
+
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var InvitationCode = mongoose.model('InvitationCode');
+
+router.get('/:content', function(req, res, next) {
+
+  let content = req.params.content;
+  InvitationCode.findOne({content: content})
+    .exec()
+    .then(function(invitation) {
+
+      if(!invitation){
+
+        res.send({state: 404, data: {}, message: ERROR_CODE});
+      } else {
+
+        res.send({state: 200, data: invitation, message: CORRECT_CODE})
+      }
+    })
+   .onReject(function(err) {
+
+      next(err);
+   })
+});
+
+module.exports = router;
