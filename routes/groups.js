df642afcfeb842e9367e7a5db4991c71f0523222
fgcui <fgcui1204@163.com>
2015-05-07 14:41:03 +0800

    moidfy session username

--- routes/groups.js
+++ routes/groups.js
@@ -7,9 +7,10 @@ var Trainee = mongoose.model('Trainee');
 
 router.get('/trainees', function (req, res, next) {
   Trainee.find({}, function (err, trainees) {
+    var currentUserName = req.session.currentUserName;
     if(err) {next(err)}
 
-    res.send(trainees);
+    res.send({trainees: trainees,currentUserName: currentUserName});
   });
 });
 
