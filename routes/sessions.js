3fb460321af4170517475595cd1506a948210bc6
fgcui <fgcui1204@163.com>
2015-05-08 17:55:46 +0800

    add get session and delete session

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,3 +1,19 @@
-/**
- * Created by fgcui on 5/8/15.
- */
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function(req,res) {
+  var currentTrainerName = req.session.currentTrainerName;
+
+  res.send(currentTrainerName);
+});
+
+router.delete('/',function(req, res) {
+  req.session.currentTrainerId = null;
+  req.session.currentTrainerName = null;
+
+  res.send({state: 200, data: {}});
+});
+
+module.exports = router;
