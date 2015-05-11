0c639f6ab07e57383fdd9bd25d3556060ca3c370
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-11 11:37:59 +0800

    add test seeds

---
+++ seeds/test/appraises.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var appraises = [
+
+  {_id: {$oid: '554983027cb6030c4268d059'}, level: 'A', appraiser: {$oid: '550240fe573a3bf20a81ac66'}, group: {$oid: '554983027cb6030c4268d059'}, comment: '挺好的', appraised_date: '2015-4-6', create_date: '2015-5-6', type : '日', __v : 0 },
+  {_id: {$oid: '554983027cb6030c4268d05a'}, level: 'B', appraiser: {$oid: '550240fe573a3bf20a81ac66'}, group: {$oid: '554983027cb6030c4268d05a'}, comment: '不错的', appraised_date: '2015-4-7', create_date: '2015-5-7', type : '日', __v : 0 }
+];
+
+module.exports = appraises;
---
+++ seeds/test/trainers.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var trainers = [
+
+  {__v: 0, _id: '550240fe573a3bf20a81ac66', username: '林文军', password: '123456', email: '915375045@qq.com', gender: '男', groups: [{$oid: '554983027cb6030c4268d059'}, {$oid: '554983027cb6030c4268d05a'}], stations: [{$oid: '550248cd0bf4a43115ef2daa'}, {$oid: '550248cd0bf4a43115ef2dab'}]}
+];
+
+module.exports = trainers;
