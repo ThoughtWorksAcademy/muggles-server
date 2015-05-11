6a9a06427e84dc158fa235314eeaac8483850312
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-11 10:03:12 +0800

    move seeds to produce

---
+++ seeds/test/invitation-code.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var invitationCodes = [
+
+{__v: 0, _id: '554861786fb722cc8a0f20b0', content: '2003967524987'}
+];
+
+module.exports = invitationCodes;
---
+++ seeds/test/trainee.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var trainees = [
+
+  {__v: 0, _id: '550249080bf4a43115ef2dae', username: '张志慧', email: '915375045@qq.com', password: '123456', gender: '女', create_date : '2015-5-3', phone_number : '18729591184', current_group : '554983027cb6030c4268d05a', groups: [{$oid: '554983027cb6030c4268d059'}, {$oid: '554983027cb6030c4268d05a'}], appraises: [{$oid: '554983027cb6030c4268d059'}, {$oid: '554983027cb6030c4268d05a'}]}
+];
+
+module.exports = trainees;
