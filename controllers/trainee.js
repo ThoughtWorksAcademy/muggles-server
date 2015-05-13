d5c2bf9060e04e20b8e10d355f5eb53675f5f154
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-13 16:00:21 +0800

    delete useless code

--- controllers/trainee.js
+++ controllers/trainee.js
@@ -68,12 +68,11 @@ var create_trainee = function (req, res, next) {
     })
 };
 
-var has_appraised = function(req, res, next) {
+var update_appraise = function(req, res, next) {
 
   var trainee_id = req.params.id;
   var current_appraise = req.body.appraise;
 
-  console.log(current_appraise);
   Trainee.findById(trainee_id)
     .populate('appraises')
     .exec()
@@ -89,15 +88,12 @@ var has_appraised = function(req, res, next) {
     })
     .then(function(appraise) {
 
-      console.log(appraise);
       return Appraise.findById(appraise.id).exec();
     })
     .then(function(appraise) {
 
-      console.log(appraise);
       appraise.comment = current_appraise.comment;
       appraise.save();
-      console.log(appraise.comment);
       res.send({state: 200, data: appraise, message: '修改成功'})
     })
     .onReject(function(err) {
@@ -105,73 +101,33 @@ var has_appraised = function(req, res, next) {
     });
 };
 
-var update_appraise_by_id = function(req, res, next) {
-
-  var trainee_id = req.params.id;
-  var current_appraise = req.body;
-  current_appraise.appraiser = req.session.currentUserId || '';
-
-  Trainee.findById(trainee_id)
-    .populate('appraises')
-    .exec()
-    .then(function(trainee) {
-
-      var new_trainee = trainee;
-      new_trainee.appraises = trainee.appraises.filter(function(appraise) {
-        return appraise.type === current_appraise.type && appraise.group.toString() === current_appraise.group;
-      });
-
-      return new_trainee;
-    })
-    .then(function(trainee) {
-
-      var result = _.find(trainee.appraises, function(appraise) {
-        return date_util.format_date(appraise) === date_util.format_date(current_appraise);
-      });
-
-      if(result) {
-        res.send({state: 200, data: true, message: APPRAISED_ALREADY})
-      } else {
-        Appraise.create(current_appraise)
-          .then(function (appraise) {
-
-            return Trainee.findById(trainee_id, function (err, trainee) {
-              trainee.appraises.push(appraise._id);
-              trainee.save();
-              res.send({state: 200, data: trainee, message: APPRAISE_ADD_SUCCESS});
-            });
-          })
-      }
-    })
-    .onReject(function(err) {
-
-      next(err);
-    });
-};
 
 var add_appraise = function(req, res, next) {
 
   var trainee_id = req.params.id;
-  var appraise = req.body;
+  var current_appraise = req.body;
 
-  appraise.appraiser = req.session.currentUserId;
+  current_appraise.appraiser = req.session.currentUserId;
   Trainee.findById(trainee_id)
     .populate('appraises')
     .exec()
-    .then(function (trainee) {
-      return _.find(trainee.appraises, function (one) {
-        console.log(date_util.find_formated_date(one) === date_util.find_formated_date(appraise));
-        return (date_util.find_formated_date(one) === date_util.find_formated_date(appraise) && one.type === appraise.type && one.group === appraise.group);
+    .then(function(trainee) {
+      return _.find(trainee.appraises, function(appraise) {
+
+        var is_the_same_day = (date_util.format_date(appraise) === date_util.format_date(current_appraise));
+        var is_the_same_group = (appraise.group.toString() === current_appraise.group);
+        var is_the_same_type = (appraise.type === current_appraise.type);
+        return is_the_same_day && is_the_same_group && is_the_same_type;
       });
     })
     .then(function (result) {
       if (result) {
 
-        res.send({state: 200, data: false, message: '此条评价已存在已评价'});
+        res.send({state: 200, data: false, message: APPRAISED_ALREADY});
       } else {
 
-        Appraise.create(appraise)
-        .then(function (appraise) {
+        Appraise.create(current_appraise)
+          .then(function (appraise) {
             Trainee.findById(trainee_id, function (err, trainee) {
               trainee.appraises.push(appraise._id);
               trainee.save();
@@ -211,8 +167,7 @@ module.exports = {
   get_trainee_by_id: get_trainee_by_id,
   verify_trainee_by_email: verify_trainee_by_email,
   create_trainee: create_trainee,
-  has_appraised: has_appraised,
-  update_appraise_by_id: update_appraise_by_id,
+  update_appraise: update_appraise,
   add_appraise: add_appraise,
   add_appraises: add_appraises
 };
