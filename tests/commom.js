4372287a642c9a3d7ed314bf2568dfff3d07c518
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-13 22:56:18 +0800

    correct momgoose model register

--- tests/commom.js
+++ tests/commom.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var fs = require('fs');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/twa-muggles-test', function (err) {
@@ -8,6 +9,11 @@ mongoose.connect('mongodb://localhost/twa-muggles-test', function (err) {
   } else {
     console.log('connection successful');
   }
+});
+
+var modules_path = '/Users/zhzhang/Documents/project/muggleProject/muggles-server/models';
+fs.readdirSync(modules_path).forEach(function (file) {
+  if (~file.indexOf('.js')) require(modules_path + '/' + file);
 });
 
 var chai = require('chai');
--- tests/helper/reloadDatabase.js
+++ tests/helper/reloadDatabase.js
@@ -3,57 +3,56 @@
 var _ = require('lodash');
 var mongoose = require('mongoose');
 
-//var Trainee = mongoose.model('Trainee');
-//var InvitationCode = mongoose.model('InvitationCode');
-//var Trainer = mongoose.model('Trainer');
-//var Group = mongoose.model('Group');
-//var Appraise = mongoose.model('Appraise');
-//
-//var invitationCodes =  require('../../seeds/test/invitation-codes');
-//var trainees = require('../../seeds/test/trainees');
-//var trainers = require('../../seeds/test/trainers');
-//var groups = require('../../seeds/test/groups');
-//var appraises = require('../../seeds/test/appraises');
-//
-//var tasks = [
-//  {operate: InvitationCode, seed: {}},
-//  {operate: Trainee, seed: {}},
-//  {operate: Trainer, seed: {}},
-//  {operate: Group, seed: {}},
-//  {operate: Appraise, seed: {}}
-//];
-//
-//var createTasks = [
-//  {operate: InvitationCode, seed: invitationCodes},
-//  {operate: Trainee, seed: trainees},
-//  {operate: Trainer, seed: trainers},
-//  {operate: Group, seed: groups},
-//  {operate: Appraise, seed: appraises}
-//];
-//
+var Trainee = mongoose.model('Trainee');
+var InvitationCode = mongoose.model('InvitationCode');
+var Trainer = mongoose.model('Trainer');
+var Group = mongoose.model('Group');
+var Appraise = mongoose.model('Appraise');
+
+var invitationCodes =  require('../../seeds/test/invitation-codes');
+var trainees = require('../../seeds/test/trainees');
+var trainers = require('../../seeds/test/trainers');
+var groups = require('../../seeds/test/groups');
+var appraises = require('../../seeds/test/appraises');
+
+var tasks = [
+  {operate: InvitationCode, seed: {}},
+  {operate: Trainee, seed: {}},
+  {operate: Trainer, seed: {}},
+  {operate: Group, seed: {}},
+  {operate: Appraise, seed: {}}
+];
+
+var createTasks = [
+  {operate: InvitationCode, seed: invitationCodes},
+  {operate: Trainee, seed: trainees},
+  {operate: Trainer, seed: trainers},
+  {operate: Group, seed: groups},
+  {operate: Appraise, seed: appraises}
+];
 
 var reloadDatabase = function (done) {
-  //var taskLen = tasks.length;
-  //var createTaskLen = createTasks.length;
-  //
-  //tasks.forEach(function(task) {
-  //
-  //  task.operate.remove(task.seed, function() {
-  //
-  //    if(0 === --taskLen) {
-  //
-  //      createTasks.forEach(function(task) {
-  //
-  //        task.operate.create(task.seed, function() {
-  //
-  //          if(0 === --createTaskLen) {
-  //            done();
-  //          }
-  //        });
-  //      });
-  //    }
-  //  });
-  //});
+  var taskLen = tasks.length;
+  var createTaskLen = createTasks.length;
+
+  tasks.forEach(function(task) {
+
+    task.operate.remove(task.seed, function() {
+
+      if(0 === --taskLen) {
+
+        createTasks.forEach(function(task) {
+
+          task.operate.create(task.seed, function() {
+
+            if(0 === --createTaskLen) {
+              done();
+            }
+          });
+        });
+      }
+    });
+  });
 };
 
 module.exports = reloadDatabase;
