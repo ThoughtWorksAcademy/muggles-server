93344124c6ffe99e612ee4d204902a4dec708a83
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-14 16:41:20 +0800

    refactor trainerController

--- routes/sessions.js
+++ routes/sessions.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-var trainer_controller = require('../models/trainer');
+var trainer_controller = require('../controllers/trainer');
 
 router.get('/', trainer_controller.login);
 router.delete('/', trainer_controller.logout);
