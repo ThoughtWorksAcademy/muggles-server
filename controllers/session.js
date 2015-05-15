70cde4feef86756489d04ca3308b7352ea8aea78
fgcui <fgcui1204@163.com>
2015-05-15 10:24:02 +0800

    modify session controller

---
+++ controllers/session.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var LOGIN_FAILURE = '邮箱或密码错误';
+var LOGIN_SUCCESS = '登录成功';
+var LOGOUT_SUCCESS = '退出成功';
+
+var mongoose = require('mongoose');
+var Trainer = mongoose.model('Trainer');
+
+var get_current_name = function(req, res) {
+  var currentTrainerName = req.session.currentTrainerName;
+
+  res.send(currentTrainerName);
+};
+
+var login = function(req, res, next) {
+  var email = req.body.user.email;
+  var password = req.body.user.password;
+  var session = req.session;
+
+  Trainer.findOne({email: email})
+    .exec()
+    .then(function(user) {
+      if(! user || user.password !== password) {
+        return res.send({state: 401, data: true, message: LOGIN_FAILURE});
+      }
+
+      session.currentTrainerId = user._id;
+      session.currentTrainerName = user.username;
+
+      res.send({state: 200, data: false, message: LOGIN_SUCCESS});
+    })
+    .onReject(function(err) {
+
+      next(err);
+    })
+};
+
+var logout = function(req, res) {
+  req.session.currentTrainerId = null;
+  req.session.currentTrainerName = null;
+
+  res.send({state: 200, data: {}, message: LOGOUT_SUCCESS});
+};
+
+module.exports = {
+  get_current_name: get_current_name,
+  login: login,
+  logout: logout
+};
+
--- routes/sessions.js
+++ routes/sessions.js
@@ -1,49 +1,14 @@
 'use strict';
 
-
-var LOGIN_FAILURE = '邮箱或密码错误';
-var LOGIN_SUCCESS = '登录成功';
-var LOGOUT_SUCCESS = '退出成功';
-
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var Trainer = mongoose.model('Trainer');
 
-router.get('/', function (req, res) {
-  var currentTrainerName = req.session.currentTrainerName;
+var session_controller = require('../controllers/session');
 
-  res.send(currentTrainerName);
-});
+router.get('/', session_controller.get_current_name);
 
-router.post('/login', function (req, res, next) {
-  var email = req.body.user.email;
-  var password = req.body.user.password;
-  var session = req.session;
+router.post('/login', session_controller.login);
 
-  Trainer.findOne({email: email})
-    .exec()
-    .then(function (user) {
-      if (!user || user.password !== password) {
-        return res.send({state: 401, data: true, message: LOGIN_FAILURE});
-      }
-
-      session.currentTrainerId = user._id;
-      session.currentTrainerName = user.username;
-
-      res.send({state: 200, data: false, message: LOGIN_SUCCESS});
-    })
-    .onReject(function (err) {
-
-      next(err);
-    })
-});
-
-router.delete('/', function (req, res) {
-  req.session.currentTrainerId = null;
-  req.session.currentTrainerName = null;
-
-  res.send({state: 200, data: {}});
-});
+router.delete('/', session_controller.logout);
 
 module.exports = router;
