167329bcedc1f943ea3d249c97bf346dbb5bbad9
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-15 10:18:19 +0800

    add a branch test of invitation code controller

--- tests/spec/invitation-code-test.js
+++ tests/spec/invitation-code-test.js
@@ -16,7 +16,7 @@ describe('invitation_code_controller', function () {
 
   describe('get_invitation_by_content', function () {
 
-    it('should get invitation code by a exist content ', function (done) {
+    it('should get invitation code by a existed content ', function (done) {
 
       var CORRECT_CODE = '邀请码正确';
       req_mock.params = {
@@ -26,7 +26,26 @@ describe('invitation_code_controller', function () {
       res_mock.send = function (object) {
 
         expect(object.state).to.equal(200);
+        expect(object.data.content).to.equal('2003967524987');
         expect(object.message).to.equal(CORRECT_CODE);
+
+        done();
+      };
+
+      invitation_code_controller.get_invitation_by_content(req_mock, res_mock);
+    });
+
+    it('should get {} by a not existed content ', function (done) {
+
+      var ERROR_CODE = '邀请码错误';
+      req_mock.params = {
+        content: '20039675249870'
+      };
+
+      res_mock.send = function (object) {
+
+        expect(object.state).to.equal(404);
+        expect(object.message).to.equal(ERROR_CODE);
 
         done();
       };
